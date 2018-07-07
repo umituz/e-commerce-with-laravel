78e383eabb293b10f66bb6770ed1395efced2d38
Ümit UZ <umituz998@gmail.com>
2018-07-07 22:08:16 +0300

    Controller oluşturulma

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Merhaba Ciğerim
+                    Nassın İymisin Ciğerim
                 </div>
             </div>
         </div>
