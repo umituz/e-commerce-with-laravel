23efbd2ce4cacc081a296d5bd04a10cc0ecba8b1
Ümit UZ <umituz998@gmail.com>
2018-07-07 23:54:27 +0300

    Alıştırmalar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,19 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view("home");
+        $isim = "Ümit";
+        $soyisim = "UZ";
+        $users = ["a","b","c","d","e","f","g","h","j"];
+        $letters = [
+            ["id" => 5,"name" => "aslan"]
+        ];
+        return view("home",compact("letters"));
+    }
+
+    public function with()
+    {
+        $isim = "Ümit";
+        $soyisim = "UZ";
+        return view("home")->with(["isim" => $isim,"soyisim" => $soyisim    ]);
     }
 }
